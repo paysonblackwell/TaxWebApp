4e70607e89348549ec21e01e064f7df1f1e19194
Payson Blackwell <paysonblackwell@gmail.com>
2018-12-02 19:49:12 -0800

    Making a PersonList, may be deleted soon
    Since we need a complex data model and this wouldn't be one

---
+++ Models/PersonList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaxWebApp.Models
+{
+    public class PersonList
+    {
+        private static List<Person> pList = new List<Person>();
+
+        public void addPerson(Person p)
+        {
+            pList.Add(p);
+        }
+
+        public void removePerson(Person p)
+        {
+            pList.Remove(p);
+        }
+
+
+    }
+}
